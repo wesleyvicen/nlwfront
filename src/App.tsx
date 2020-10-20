8bb15d15fea7ad9b81f3ba39e1427c6136f19070
wesleyvicen <wesley1535@hotmail.com>
2020-10-20 15:01:30 -0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import './styles/global.css';
-import './styles/pages/landing.css';
 
-import {FiArrowRight} from 'react-icons/fi';
-
-import logoImg from './images/logo.svg';
+import Routes from './routes'
 
 function App() {
   return (
-    <div id={'page-landing'}>
-     <div className="content-wrapper">
-      <img src={logoImg} alt="Happy"/>
-
-      <main>
-        <h1>Leve felicidade para o mundo</h1>
-        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        
-
-      </main>
-      <div className="location">
-          <strong>Vicência</strong>
-          <span>Pernambuco</span>
-        </div>
-      <a href="/#" className="enter-app"><FiArrowRight size={26} color={'rgba(0,0,0,0.6)'}/></a>
-
-     </div>
-    </div>
+    <Routes/>
   );
 }
 
