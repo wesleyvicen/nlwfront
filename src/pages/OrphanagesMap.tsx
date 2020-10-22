9956b40494a52a4ba8e67ecee291030e4878b23b
Wesley Vicente <wesley1535@hotmail.com>
2020-10-22 13:03:10 -0500

    front finalizado ate ultima ordem

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-
-import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap(){
+const [latitude, setLatitude] = useState(0);
+const [longitude, setLongitude] = useState(0);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      { timeout: 30000 }
+    );
+  }, []);
+
   return (
   <div id="page-map">
     <aside>
@@ -21,9 +38,14 @@ function OrphanagesMap(){
         <span>Pernambuco</span>
         </footer>
     </aside>
-    <div></div>
-
-    <Link to={''} className={'create-orphanage'}><FiPlus size={32} color={'#FFF'}/></Link>
+    <Map
+    center={[latitude, longitude]}
+    zoom={15}
+    style={{width:'100%', height:'100%'}}
+    >
+      <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+    </Map>
+        <Link to={''} className={'create-orphanage'}><FiPlus size={32} color={'#FFF'}/></Link>
   </div>
   );
 }
