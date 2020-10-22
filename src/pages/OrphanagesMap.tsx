2c1bb27049991ed2a2ef20bc73ec9eba81ec054e
Wesley Vicente <wesley1535@hotmail.com>
2020-10-22 15:48:23 -0300

    arrumado erro no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
 
 import mapMarkerImg from '../images/map-marker.svg'
 
@@ -24,6 +26,18 @@ const [longitude, setLongitude] = useState(0);
       { timeout: 30000 }
     );
   }, []);
+  
+  function CarregaMap(){
+    return(
+    <Map
+    center={[-8.0398072, -34.961686]}
+    zoom={15}
+    style={{width:'100%', height:'100%'}}
+    >
+      <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+    </Map>
+    )
+  }
 
   return (
   <div id="page-map">
@@ -38,13 +52,7 @@ const [longitude, setLongitude] = useState(0);
         <span>Pernambuco</span>
         </footer>
     </aside>
-    <Map
-    center={[latitude, longitude]}
-    zoom={15}
-    style={{width:'100%', height:'100%'}}
-    >
-      <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-    </Map>
+    <CarregaMap/>
         <Link to={''} className={'create-orphanage'}><FiPlus size={32} color={'#FFF'}/></Link>
   </div>
   );
